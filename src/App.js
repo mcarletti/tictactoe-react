25fef9722fe7113c03f71106a48cf3c177cffbf7
Marco Carletti <marco.carletti@evsys.it>
2023-06-12 11:14:17 +0200

    added: comments to app component; updated: images, that is logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import "./App.css";
 import "./style.css";
 import React, { useState } from "react"
-import logo from "./images/github-mark-white.png"
 
 function Title()
 {
+  // this is the simplest React component
+  // which returns a title with a link;
+  // each component must return a single element
   return <h1><a href="">Tic Tac Toe</a></h1>
 }
 
 function Cell({ value, onCellClick })
 {
+  // the tic-tac-toe grid is made of 9 cells
+  // each cell is a component that can be clicked
+  // to mark it with an X or an O
   return <div className="cell" onClick={onCellClick}>{value}</div>
 }
 
 function Grid()
 {
+  // the grid component is the actual game;
+  // it keeps track of the game state and it renders the cells
+
+  // 'useState' is a React hook that allows to use state in a functional component
+  // its argument is the initial state of the component; it returns an array with
+  // the current state and a function to update it
+
+  // who is the next player?
   const [xIsNext, setXIsNext] = useState(true);
+  // state of the grid (9 cells)
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function calculateWinner(squares)
   {
+    // check if there is a winner
+    // if so, return the winner (X or O)
+    // otherwise, return null
+
     const lines = [
       [0, 1, 2], // horizontal
       [3, 4, 5], // horizontal
@@ -43,21 +61,29 @@ function Grid()
 
   function handleClick(i)
   {
+    // when a cell is clicked, update the grid state
+    // and change the next player
+
     if (calculateWinner(squares) || squares[i]) return;
 
+    // to avoid mutating the state directly, we create a copy of the array;
+    // this is useful to implement the 'undo' feature
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
 
+  // every time the grid is rendered, check if there is a winner
   const winner = calculateWinner(squares);
   const status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O");
 
+  // render the grid
   return <>
     <div className="status">{status}</div>
     <div className="grid">
       <div className="row">
+        {/* we pass a cell its own state; if clicked, the handler is triggered with the cell id */}
         <Cell value={squares[0]} onCellClick={() => handleClick(0)} />
         <Cell value={squares[1]} onCellClick={() => handleClick(1)} />
         <Cell value={squares[2]} onCellClick={() => handleClick(2)} />
@@ -76,22 +102,28 @@ function Grid()
   </>
 }
 
-function Copyright({n, c=0}) {
+function Copyright()
+{
+  // return a paragraph with the current year
   return <p className="copyright">Marco Carletti &copy; 2023</p>
 }
 
-function App() {
+function App()
+{
+  // build the app interface by composing components
 
   return (
+    // 'className' is like class in HTML, but it's used in JSX
+    // 'class' is not used because it's a reserved word in JS
     <div className="App">
       <header className="App-header">
         <Title />
         <Grid />
         <Copyright />
-        <a href="https://github.com/mcarletti/tictactoe-react"><img src={logo} className="github-logo" alt="github-logo" /></a>
       </header>
     </div>
   );
 }
 
+// make this component available to external world
 export default App;
